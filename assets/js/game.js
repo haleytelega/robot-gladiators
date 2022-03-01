8eb81b2a1273512843e6f2dbad62bca2cc25306d
Haley Telega <haleytelega@outlook.com>
2022-02-28 21:59:31 -0500

    Added more to js file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! JavaScript is running!");
+//this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+var playerName = window.prompt("What is your robots name?");
+
+console.log(playerName);
+
+console.log("This logs a sting, good for leaving yorself a message");
+console.log(10 + 10);
+console.log("Our rebots name is" + playerName)
+// fight ();
